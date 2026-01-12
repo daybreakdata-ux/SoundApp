fcdba9a59e5fb2fc202c44136d17f4b6a75810c5
Daybreak Data <daybreakdata@example.com>
2026-01-12 04:56:11 -0500

    2

---
+++ app/components/index.ts
@@ -0,0 +1,8 @@
+// Barrel file for components
+export { default as AudioControls } from "./AudioControls";
+export { default as BoostControl } from "./BoostControl";
+export { default as DeviceSelector } from "./DeviceSelector";
+export { default as EQBand } from "./EQBand";
+export { default as EqualizerSection } from "./EqualizerSection";
+export { default as NoiseSuppressionToggle } from "./NoiseSuppressionToggle";
+export { default as VolumeControl } from "./VolumeControl";
